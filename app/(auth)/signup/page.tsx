ace4b7610c0eab31de4e2dc9908ffc583cdc0968
EngJao89 <eng_joao@outlook.com>
2025-01-10 21:12:00 -0300

    feat(da03-forms-control): implements control of infos fields

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,46 +1,80 @@
-import { Pressable, StyleSheet, Text, TextInput, View } from "react-native";
+import { useState } from "react";
+import { Pressable, SafeAreaView, ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
 import { router } from "expo-router"
 import { Ionicons } from "@expo/vector-icons";
 
 import { Colors } from "@/constants/colors";
 
 export default function SignUp() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState();
+
+  function handleSignUp() {
+    console.log({
+      name,
+      email,
+      password
+    });
+  }
+
   return(
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Pressable style={styles.backButton} onPress={() => router.back()}>
-          <Ionicons name="arrow-back" size={24} color={Colors.white}/>
-        </Pressable>
-        <Text style={styles.logoText}>
-          Dev<Text style={{color: Colors.green}}>App</Text>
-        </Text>
+    <SafeAreaView style={{ flex: 1, backgroundColor: Colors.zinc }}>
+      <ScrollView style={{ flex: 1, backgroundColor: Colors.white }}>
+        <View style={styles.container}>
+          <View style={styles.header}>
+            <Pressable style={styles.backButton} onPress={() => router.back()}>
+              <Ionicons name="arrow-back" size={24} color={Colors.white}/>
+            </Pressable>
+            <Text style={styles.logoText}>
+              Dev<Text style={{color: Colors.green}}>App</Text>
+            </Text>
 
-        <Text style={styles.slogan}>
-          Criar uma conta
-        </Text>
-      </View>
+            <Text style={styles.slogan}>
+              Criar uma conta
+            </Text>
+          </View>
 
-      <View style={styles.form}>
-        <View>
-          <Text style={styles.label}>Nome Completo</Text>
-          <TextInput placeholder='Digite seu nome...' style={styles.input} />
+          <View style={styles.form}>
+            <View>
+              <Text style={styles.label}>Nome Completo</Text>
+              <TextInput 
+                placeholder='Digite seu nome...' 
+                value={name}
+                onChangeText={setName}
+                style={styles.input} 
+              />
+            </View>
+
+            <View>
+              <Text style={styles.label}>Email</Text>
+              <TextInput 
+                placeholder='Digite seu email...' 
+                value={email}
+                onChangeText={setEmail}
+                style={styles.input} 
+              />
+            </View>
+
+            <View>
+              <Text style={styles.label}>Senha</Text>
+              <TextInput 
+                placeholder='Digite sua senha...' 
+                secureTextEntry 
+                value={password}
+                onChangeText={setPassword}
+                style={styles.input} 
+              />
+            </View>
+
+            <Pressable style={styles.button} onPress={handleSignUp}>
+              <Text style={styles.buttonText}>Cadastrar</Text>
+            </Pressable>
+          </View>
         </View>
-
-        <View>
-          <Text style={styles.label}>Email</Text>
-          <TextInput placeholder='Digite seu email...' style={styles.input} />
-        </View>
-
-        <View>
-          <Text style={styles.label}>Senha</Text>
-          <TextInput placeholder='Digite sua senha...' secureTextEntry style={styles.input} />
-        </View>
-
-        <Pressable style={styles.button}>
-          <Text style={styles.buttonText}>Cadastrar</Text>
-        </Pressable>
-      </View>
-    </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,18 @@
 import { Stack } from "expo-router";
+import { StatusBar } from 'expo-status-bar';
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{headerShown: false}}/>
+    <>
+      <Stack>
+        <Stack.Screen name="index" options={{headerShown: false}}/>
 
-      <Stack.Screen name="(auth)/signup/page" options={{headerShown: false}}/>
+        <Stack.Screen name="(auth)/signup/page" options={{headerShown: false}}/>
 
-      <Stack.Screen name="(panel)/profile/page" options={{headerShown: false}}/>
-    </Stack>
+        <Stack.Screen name="(panel)/profile/page" options={{headerShown: false}}/>
+      </Stack>
+
+      <StatusBar style="dark" />
+    </>
   )
 }
